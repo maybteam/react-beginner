5c8345626409e9d60c89ffbc10a058d4fb3e80e7
rldnd <gi981226@gmail.com>
2022-11-02 16:52:50 +0900

    feat(section4): variable

---
+++ section4/variable.js
@@ -0,0 +1,12 @@
+var varName = "name";
+let letName = "name";
+const constName = "name";
+
+letName = "letName";
+
+// NOTE: ERROR
+// constName = "constName";
+
+console.log("varName::", varName);
+console.log("letName::", letName);
+console.log("constName::", constName);
